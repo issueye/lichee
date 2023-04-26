b8ace300662dc02357be1160250a002821c7e44c
issueye <issueye@yeah.net>
2023-04-26 21:50:02 +0800

    修改结构体

--- app/model/db_model.go
+++ app/model/db_model.go
@@ -15,27 +15,27 @@ type DbSource struct {
 // ReqCreateDbSource
 // 创建
 type ReqCreateDbSource struct {
-	Name     string `json:"name" binding:"required" label:"名称" example:"测试SQLSERVER_001"`     // 名称
-	Host     string `json:"host" binding:"required" label:"服务地址" example:"192.168.200.66/gy"` // 地址
-	Port     int64  `json:"port" binding:"required" label:"端口号" example:"1433"`               // 端口号
-	Database string `json:"database" binding:"required" label:"数据库名称" example:"TEST"`         // 数据库
-	User     string `json:"user" binding:"required" label:"账户" example:"sa"`                  // 账号
-	Password string `json:"password" binding:"required" label:"密码" example:"123456"`          // 密码
-	Type     int64  `json:"type" label:"数据库类型" example:"0"`                                   // 类型
-	Mark     string `json:"mark"`                                                             // 备注
+	Name     string `json:"name" binding:"required" label:"名称" example:"测试SQLSERVER_001"` // 名称
+	Host     string `json:"host" binding:"required" label:"服务地址" example:"."`             // 地址
+	Port     int64  `json:"port" binding:"required" label:"端口号" example:"1433"`           // 端口号
+	Database string `json:"database" binding:"required" label:"数据库名称" example:"TEST"`     // 数据库
+	User     string `json:"user" binding:"required" label:"账户" example:"sa"`              // 账号
+	Password string `json:"password" binding:"required" label:"密码" example:"123456"`      // 密码
+	Type     int64  `json:"type" label:"数据库类型" example:"0"`                               // 类型
+	Mark     string `json:"mark"`                                                         // 备注
 }
 
 // 修改
 type ReqModifyDbSource struct {
-	Id       int64  `json:"id"  binding:"required" label:"id" example:"10000"`                // ID
-	Name     string `json:"name" binding:"required" label:"名称" example:"测试SQLSERVER_001"`     // 名称
-	Host     string `json:"host" binding:"required" label:"服务地址" example:"192.168.200.66/gy"` // 地址
-	Port     int64  `json:"port" binding:"required" label:"端口号" example:"1433"`               // 端口号
-	Database string `json:"database" binding:"required" label:"数据库名称" example:"TEST"`         // 数据库
-	User     string `json:"user" binding:"required" label:"账户" example:"sa"`                  // 账号
-	Password string `json:"password" binding:"required" label:"密码" example:"123456"`          // 密码
-	Type     int64  `json:"type" label:"数据库类型" example:"0"`                                   // 类型
-	Mark     string `json:"mark"`                                                             // 备注
+	Id       int64  `json:"id"  binding:"required" label:"id" example:"10000"`            // ID
+	Name     string `json:"name" binding:"required" label:"名称" example:"测试SQLSERVER_001"` // 名称
+	Host     string `json:"host" binding:"required" label:"服务地址" example:"."`             // 地址
+	Port     int64  `json:"port" binding:"required" label:"端口号" example:"1433"`           // 端口号
+	Database string `json:"database" binding:"required" label:"数据库名称" example:"TEST"`     // 数据库
+	User     string `json:"user" binding:"required" label:"账户" example:"sa"`              // 账号
+	Password string `json:"password" binding:"required" label:"密码" example:"123456"`      // 密码
+	Type     int64  `json:"type" label:"数据库类型" example:"0"`                               // 类型
+	Mark     string `json:"mark"`                                                         // 备注
 }
 
 // ReqQueryDbSource
